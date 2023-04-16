29dc35e0910a1bfb9402269f587577c6c46aa077
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2023-04-16 14:23:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,41 @@ close0.addEventListener('click', () => {
     list.style.top = '10vh';
     list.style.left = '-100vw';
 })
+
+
+//Slider
+const carouselContainer = document.querySelector('.carousel-container');
+const cards = document.querySelectorAll('.card');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let currentCard = 0;
+
+function showCard(index) {
+  carouselContainer.style.transform = `translateX(-${index * 320}px)`;
+}
+
+function setActiveCard() {
+  cards.forEach((card) => {
+    card.classList.remove('active');
+  });
+  cards[currentCard].classList.add('active');
+}
+
+prevBtn.addEventListener('click', () => {
+  currentCard--;
+  if (currentCard < 0) {
+    currentCard = cards.length - 1;
+  }
+  showCard(currentCard);
+  setActiveCard();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentCard++;
+  if (currentCard > cards.length) {
+      currentCard = cards.length + 1;
+  }
+  showCard(currentCard);
+  setActiveCard();
+});
